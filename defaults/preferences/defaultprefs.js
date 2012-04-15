2103227ae79c4f92f00cb742c89892e99b500ed5
Jonathan Bisson <bissonjonathan@gmail.com>
2012-04-15 13:57:30 +0200

    Adding extensions.autozotbib.automatic parameter, allowing to disable autozotbib temporarly.

--- defaults/preferences/defaultprefs.js
+++ defaults/preferences/defaultprefs.js
@@ -1,2 +1,3 @@
 pref("extensions.autozotbib.bibtex_filename", "/Users/robin/ZotOutput.bib");
+pref("extensions.autozotbib.automatic", true);
 pref("extensions.autozotbib.bibtex_translator_guid", "9cb70025-a888-4a29-a210-93ec52da40d4");
