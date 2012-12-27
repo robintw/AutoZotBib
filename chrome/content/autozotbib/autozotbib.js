999776fbd48e58218b8fc8ed3090052e1c828285
Robin Wilson <robin@rtwilson.com>
2012-12-27 10:24:34 +0000

    Moved all global vars into properties of the Zotero.AutoZotBib class.

--- chrome/content/autozotbib/autozotbib.js
+++ chrome/content/autozotbib/autozotbib.js
@@ -1,28 +1,6 @@
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
 
-var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.autozotbib.");
-var prefs_window_ref;
-
-var data;
-
-var events_id = [];
-var events_type = [];
-var events_timestamp = [];
-
-var okToWriteToBibtex = true;
-var needToAppendStill = false;
-
-var lastTimestamp;
-var timerRunning = false;
-
-
-const TYPE_REPEATING_PRECISE_CAN_SKIP = Components.interfaces.nsITimer.TYPE_REPEATING_PRECISE_CAN_SKIP;
-
-const DIFF_DO_NOTHING = 1;
-const DIFF_CLEAR_QUEUE = 5;
-const DIFF_STOP_TIMER = 20;
-
 var prefsEvent = {
 	observe: function(subject, topic, data) {
      if (topic != "nsPref:changed")
@@ -38,7 +16,7 @@ var prefsEvent = {
 	     	// Thus we need to do a full export - so that we can then add/remove
 	     	// things from it.
 	     	// (but only if we're in automatic mode - otherwise we shouldn't touch it!)
-	     	if (prefs.getBoolPref('automatic'))
+	     	if (Zotero.AutoZotBib.prefs.getBoolPref('automatic'))
 	     	{
 	     		Zotero.AutoZotBib.exportAll();
 	     	}
@@ -47,7 +25,7 @@ var prefsEvent = {
 	    	// If we have just switched the automatic mode on
 	    	// then do a full export - as we don't know what
 	    	// happened while it was off!
-	    	if (prefs.getBoolPref('automatic'))
+	    	if (Zotero.AutoZotBib.prefs.getBoolPref('automatic'))
 	    	{
 	    		Zotero.AutoZotBib.exportAll();
 	    	}
@@ -59,7 +37,7 @@ var prefsEvent = {
 // ... interface for the callbacks.
 var timerEvent = {
   queueEmpty: function() {
-  	if (events_timestamp.length == 0)
+  	if (Zotero.AutoZotBib.events_timestamp.length == 0)
   	{
   		return(true);
   	}
@@ -70,27 +48,27 @@ var timerEvent = {
   },
 
   observe: function(subject, topic, data) {
-  	if (events_timestamp.length > 0)
+  	if (Zotero.AutoZotBib.events_timestamp.length > 0)
   	{
-  		lastTimestamp = events_timestamp[events_timestamp.length - 1];
+  		Zotero.AutoZotBib.lastTimestamp = Zotero.AutoZotBib.events_timestamp[Zotero.AutoZotBib.events_timestamp.length - 1];
   	}
     currentTimestamp = new Date().getTime() / 1000;
 
-    diff = currentTimestamp - lastTimestamp;
-    dump("Fired timer\tCurrent: " + currentTimestamp + "\tLast: " + lastTimestamp + "\tDiff: " + diff + "\n");
-    if (diff < DIFF_DO_NOTHING)
+    diff = currentTimestamp - Zotero.AutoZotBib.lastTimestamp;
+    dump("Fired timer\tCurrent: " + currentTimestamp + "\tLast: " + Zotero.AutoZotBib.lastTimestamp + "\tDiff: " + diff + "\n");
+    if (diff < Zotero.AutoZotBib.DIFF_DO_NOTHING)
     {
     	return;
     }
-    if (diff > DIFF_CLEAR_QUEUE && diff < DIFF_STOP_TIMER && this.queueEmpty() == false)
+    if (diff > Zotero.AutoZotBib.DIFF_CLEAR_QUEUE && diff < Zotero.AutoZotBib.DIFF_STOP_TIMER && this.queueEmpty() == false)
     {
     	// Process queue, clear queue and process items
     	var ids = Zotero.AutoZotBib.processQueue();
 
     	dump("Clearing queue\n");
-    	events_id.length = 0;
-    	events_type.length = 0;
-    	events_timestamp.length = 0;
+    	Zotero.AutoZotBib.events_id.length = 0;
+    	Zotero.AutoZotBib.events_type.length = 0;
+    	Zotero.AutoZotBib.events_timestamp.length = 0;
 
     	dump("About to process items:\t");
     	dump(ids);
@@ -98,20 +76,40 @@ var timerEvent = {
     	dump("----------------------------------------------------------------\n");
     	Zotero.AutoZotBib.processItems(ids);
     }
-    if (diff > DIFF_STOP_TIMER)
+    if (diff > Zotero.AutoZotBib.DIFF_STOP_TIMER)
     {
     	// Stop timer
     	dump("Stopping timer\n");
     	Zotero.AutoZotBib.timer.cancel();
-    	timerRunning = false;
+    	Zotero.AutoZotBib.timerRunning = false;
     }
   }
 }
 
-var search_results = [];
-
 Zotero.AutoZotBib = {
 	timer: Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer),
+
+	prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.autozotbib."),
+	prefs_window_ref: 0,
+
+	TYPE_REPEATING_PRECISE_CAN_SKIP: Components.interfaces.nsITimer.TYPE_REPEATING_PRECISE_CAN_SKIP,
+
+	DIFF_DO_NOTHING: 1,
+	DIFF_CLEAR_QUEUE: 5,
+	DIFF_STOP_TIMER: 20,
+
+	events_id: [],
+	events_type: [],
+	events_timestamp: [],
+
+	okToWriteToBibtex: true,
+	needToAppendStill: false,
+
+	lastTimestamp: undefined,
+	timerRunning: false,
+
+	search_results: [],
+
 
 	init: function () {		
 		// Register the callback in Zotero as an item observer
@@ -125,8 +123,8 @@ Zotero.AutoZotBib = {
 		Zotero.addShutdownListener(this.onShutdown);
 
 
-		prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-		prefs.addObserver("", prefsEvent, false);
+		this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		this.prefs.addObserver("", prefsEvent, false);
 	},
 
 	onShutdown: function() {
@@ -134,15 +132,15 @@ Zotero.AutoZotBib = {
 		// If Zotero shuts down we need to clear the queue
 		// and process it all - regardless of the timer
 		var ids = Zotero.AutoZotBib.processQueue();
-		events_id.length = 0;
-    	events_type.length = 0;
-    	events_timestamp.length = 0;
+		this.events_id.length = 0;
+    	this.events_type.length = 0;
+    	this.events_timestamp.length = 0;
     	Zotero.AutoZotBib.processItems(ids);
 	},
 	
 	preferences: function(w) {
-    	if (! prefs_window_ref || prefs_window_ref.closed) prefs_window_ref = w.open("chrome://autozotbib/content/preferences.xul", "", "centerscreen,chrome,dialog,resizable");
-    	else prefs_window_ref.focus();
+    	if (! this.prefs_window_ref || this.prefs_window_ref.closed) this.prefs_window_ref = w.open("chrome://autozotbib/content/preferences.xul", "", "centerscreen,chrome,dialog,resizable");
+    	else this.prefs_window_ref.focus();
   	},
 	
 	/* Process the queue of events
@@ -154,10 +152,10 @@ Zotero.AutoZotBib = {
   		// queue.
 
   		// First ensure all itemIDs are numbers (some weren't for some reason)
-  		events_id = events_id.map(function(x) {return Number(x);});
+  		this.events_id = this.events_id.map(function(x) {return Number(x);});
 
   		// Get the unique ones
-  		unique_ids = this.uniqueElements(events_id);
+  		unique_ids = this.uniqueElements(this.events_id);
 
   		return(unique_ids);
   	},
@@ -190,7 +188,7 @@ Zotero.AutoZotBib = {
 
 		var translation = new Zotero.Translate.Export();
 		translation.setItems(items);
-		trans_guid = prefs.getCharPref("bibtex_translator_guid");
+		trans_guid = this.prefs.getCharPref("bibtex_translator_guid");
 		translation.setTranslator(trans_guid);
 		translation.setHandler("done", this._appendToFileCallback);
 		translation.translate();
@@ -205,10 +203,10 @@ Zotero.AutoZotBib = {
 			dump("Error exporting items to BibTeX.\n");
 			window.alert("Error exporting items to BibTeX.");
 		} else {
-			var data = obj.string;
+			var bibtexString = obj.string;
 
 			var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-			var filename = prefs.getCharPref("bibtex_filename");
+			var filename = this.prefs.getCharPref("bibtex_filename");
 			file.initWithPath(filename);
 
 			// You can also optionally pass a flags parameter here. It defaults to
@@ -218,7 +216,7 @@ Zotero.AutoZotBib = {
 			var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
 			                createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
 			converter.charset = "UTF-8";
-			var istream = converter.convertToInputStream(data);
+			var istream = converter.convertToInputStream(bibtexString);
 			 
 			// The last argument (the callback) is optional.
 			NetUtil.asyncCopy(istream, ostream, function(status) {
@@ -238,11 +236,11 @@ Zotero.AutoZotBib = {
 	and year from the Bibtex file specified in the preferences
 	*/
   	removeBibtexEntries: function(authors, years) {
-  		okToWriteToBibtex = false;
+  		this.okToWriteToBibtex = false;
 		var file = Components.classes["@mozilla.org/file/local;1"].
 	           createInstance(Components.interfaces.nsILocalFile);
 
-	    var filename = prefs.getCharPref("bibtex_filename");
+	    var filename = this.prefs.getCharPref("bibtex_filename");
 		file.initWithPath(filename);
 
 
@@ -253,23 +251,23 @@ Zotero.AutoZotBib = {
 		    return;
 		  }
 		  // Read file into a string variable called data
-		  data = NetUtil.readInputStreamToString(inputStream, inputStream.available());
+		  fileString = NetUtil.readInputStreamToString(inputStream, inputStream.available());
 
 
 		  // Add a new fake entry starting with @ at the end so that the regexp
 		  // works for the final entry in the file
-		  data = data + "\n\n@REPLACETHIS"
+		  fileString = fileString + "\n\n@REPLACETHIS"
 
 		  // For every author and year given, remove those from the string
 		  for (i in authors)
 		  {
 		  	// Remove the BibTeX entry given as arguments to this function
 		  	// by using a regexp
-		  	data = data.replace(new RegExp('@[^@]+?author = \{(' + authors[i] + '),[^@]+?year = \{(' + years[i] + ')\}[^@]+?(?=@)','g'), "")
+		  	fileString = fileString.replace(new RegExp('@[^@]+?author = \{(' + authors[i] + '),[^@]+?year = \{(' + years[i] + ')\}[^@]+?(?=@)','g'), "")
 		  }
 
 		  // Remove the @REPLACETHIS bit as it is invalid BibTex!
-		  data = data.replace('@REPLACETHIS', '')
+		  fileString = fileString.replace('@REPLACETHIS', '')
 
 		  // You can also optionally pass a flags parameter here. It defaults to
 		  // FileUtils.MODE_WRONLY | FileUtils.MODE_CREATE | FileUtils.MODE_TRUNCATE;
@@ -278,7 +276,7 @@ Zotero.AutoZotBib = {
 		  var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
 		                createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
 		  converter.charset = "UTF-8";
-		  var istream = converter.convertToInputStream(data);
+		  var istream = converter.convertToInputStream(fileString);
 		 
 		  // The last argument (the callback) is optional.
 		  NetUtil.asyncCopy(istream, ostream, function(status) {
@@ -291,14 +289,14 @@ Zotero.AutoZotBib = {
 		  // Data has been written to the file.
 		  dump("BibTeX entries have been removed.\n");
 
-		  if (needToAppendStill)
+		  if (this.needToAppendStill)
 		  {
 		  	// Export all of the entries that we found in the search and
 			// append to the file
 			dump("\nAbout to append items to file.\n");
-			Zotero.AutoZotBib.appendItemsToFile(search_results);
+			Zotero.AutoZotBib.appendItemsToFile(this.search_results);
 		  }
-		  okToWriteToBibtex = true;
+		  this.okToWriteToBibtex = true;
 		});
 		});
 	},
@@ -311,7 +309,7 @@ Zotero.AutoZotBib = {
 		file.initWithPath(filename);
 		
 		var translator = new Zotero.Translate('export');
-		trans_guid = prefs.getCharPref("bibtex_translator_guid");
+		trans_guid = this.prefs.getCharPref("bibtex_translator_guid");
 		//translator.setTranslator('DA47106C-1265-4D19-8E75-0A4CD77CD369'); // BibTeX
 		translator.setTranslator(trans_guid);
 		translator.setItems(items);
@@ -321,7 +319,7 @@ Zotero.AutoZotBib = {
 
 	exportAll: function() {
 		var all_items = Zotero.Items.getAll(true)
-		var filename = prefs.getCharPref("bibtex_filename");
+		var filename = this.prefs.getCharPref("bibtex_filename");
 
 		this.exportItems(all_items, filename);
   	},
@@ -397,29 +395,29 @@ Zotero.AutoZotBib = {
 				continue;
 			}
 
-			search_results = search_results.concat(results);
+			this.search_results = this.search_results.concat(results);
 		}
 
-		search_results = this.uniqueElements(search_results);
+		this.search_results = this.uniqueElements(this.search_results);
 
 		dump("All search results are:\n");
-		dump(search_results);
+		dump(this.search_results);
 
-		if (search_results.length == 0)
+		if (this.search_results.length == 0)
 		{
 			return;
 		}
 
-		if (okToWriteToBibtex)
+		if (this.okToWriteToBibtex)
 		{
 			// Export all of the entries that we found in the search and
 			// append to the file
 			dump("\nAbout to append items to file.\n");
-			this.appendItemsToFile(search_results);
+			this.appendItemsToFile(this.search_results);
 		}
 		else
 		{
-			needToAppendStill = true;
+			this.needToAppendStill = true;
 		}
 
 		
@@ -429,7 +427,7 @@ Zotero.AutoZotBib = {
 	notifierCallback: {
 	    notify: function(event, type, ids, extraData) {
 
-		if (prefs.getBoolPref("automatic") == false || prefs.getCharPref("bibtex_filename") == "")
+		if (Zotero.AutoZotBib.prefs.getBoolPref("automatic") == false || Zotero.AutoZotBib.prefs.getCharPref("bibtex_filename") == "")
 		{
 			return;
 		}
@@ -453,31 +451,31 @@ Zotero.AutoZotBib = {
 		// we are dealing with a item (we don't care about collections, tags etc)
 		if (ids.length > 0 && type == 'item')
 		{
-			events_timestamp.push(secs);
-			events_type.push(event);
+			Zotero.AutoZotBib.events_timestamp.push(secs);
+			Zotero.AutoZotBib.events_type.push(event);
 
 			if (ids.length > 1)
 			{
-				events_id.concat(ids);
+				Zotero.AutoZotBib.events_id.concat(ids);
 			}
 			else
 			{
-				events_id.push(ids);
+				Zotero.AutoZotBib.events_id.push(ids);
 			}
 
-			dump(events_timestamp.join());
+			dump(Zotero.AutoZotBib.events_timestamp.join());
 			dump("\n");
-			dump(events_type.join());
+			dump(Zotero.AutoZotBib.events_type.join());
 			dump("\n");
-			dump(events_id.join());
+			dump(Zotero.AutoZotBib.events_id.join());
 			dump("\n");
 			dump("\n");
 		}
 		
-		if (timerRunning == false)
+		if (Zotero.AutoZotBib.timerRunning == false)
 		{
-			Zotero.AutoZotBib.timer.init(timerEvent, 1000, TYPE_REPEATING_PRECISE_CAN_SKIP);
-			timerRunning = true;
+			Zotero.AutoZotBib.timer.init(timerEvent, 1000, Zotero.AutoZotBib.TYPE_REPEATING_PRECISE_CAN_SKIP);
+			Zotero.AutoZotBib.timerRunning = true;
 		}
 
 	    }
