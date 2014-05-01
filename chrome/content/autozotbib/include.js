40b4134cdce16a21ace5437e319a95f7d74551a1
ReichenHack <Emiliano.Heyns@iris-advies.nl>
2014-05-01 23:07:45 +0200

    Other plugins declare loader too
    Declaring to const causes a loading order problem.

--- chrome/content/autozotbib/include.js
+++ chrome/content/autozotbib/include.js
@@ -1,6 +1,6 @@
 // Only create main object once
 if (!Zotero.AutoZotBib) {
-	const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+	var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
 	loader.loadSubScript("chrome://autozotbib/content/autozotbib.js");
 }
